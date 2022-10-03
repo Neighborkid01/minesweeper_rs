1bb59243e7c25374f6d176046efe5325c4057282
Caleb Roland <calebproland@gmail.com>
2022-10-03 15:57:09 -0400

    Barely functional

---
+++ src/cell.rs
@@ -0,0 +1,122 @@
+use std::collections::HashSet;
+use gloo_console as console;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Value {
+    Mine,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum DisplayState {
+    Default,
+    Unknown,
+    Flagged,
+    Shown,
+}
+
+#[derive(Clone, Copy)]
+pub struct Cell {
+    pub value: Value,
+    pub display: DisplayState,
+}
+
+impl Cell {
+    pub fn new(value: Value) -> Self {
+        Self {
+            value,
+            display: DisplayState::Default,
+        }
+    }
+
+    // pub fn new_empty() -> Self {
+    //     Cell::new(Value::Zero)
+    // }
+
+    pub fn handle_click(&mut self) {
+        match self.display {
+            DisplayState::Default | DisplayState::Unknown => {
+                self.display = DisplayState::Shown;
+            },
+            DisplayState::Flagged | DisplayState::Shown => {
+                self.display = self.display;
+            }
+        }
+        // console::log!("in handle_click, shown: {}", self.shown);
+        // console::log!("in handle_click, val_str: {}", self.get_value_string());
+    }
+
+    pub fn cycle_display(&mut self) {
+        match self.display {
+            DisplayState::Default => { self.set_display(DisplayState::Flagged) }
+            DisplayState::Flagged => { self.set_display(DisplayState::Unknown) }
+            DisplayState::Unknown => { self.set_display(DisplayState::Default) }
+            DisplayState::Shown   => { self.set_display(DisplayState::Shown) }
+        }
+    }
+
+    pub fn is_shown(&self) -> bool {
+        self.display == DisplayState::Shown
+    }
+
+    pub fn is_mine(&self) -> bool {
+        self.value == Value::Mine
+    }
+
+    pub fn set_value(&mut self, value: Value) {
+        self.value = value;
+    }
+
+    pub fn set_display(&mut self, display: DisplayState) {
+        self.display = display;
+    }
+
+    pub fn calculate_value(index: usize, neighbors: &HashSet<usize>, mines: &HashSet<usize>) -> Value {
+        if mines.contains(&index) { return Value::Mine }
+
+        let neighboring_mines = neighbors.intersection(&mines).count();
+        match neighboring_mines {
+            0 => { Value::Zero },
+            1 => { Value::One },
+            2 => { Value::Two },
+            3 => { Value::Three },
+            4 => { Value::Four },
+            5 => { Value::Five },
+            6 => { Value::Six },
+            7 => { Value::Seven },
+            8 => { Value::Eight },
+            _ => panic!(),
+        }
+    }
+
+    pub fn get_value_string(&self) -> &str {
+        let res = match self.display {
+            DisplayState::Default => " ",
+            DisplayState::Flagged => "🚩",
+            DisplayState::Unknown => "?",
+            DisplayState::Shown => {
+                match self.value {
+                    Value::Mine => "*",
+                    Value::Zero => " ",
+                    Value::One => "1",
+                    Value::Two => "2",
+                    Value::Three => "3",
+                    Value::Four => "4",
+                    Value::Five => "5",
+                    Value::Six => "6",
+                    Value::Seven => "7",
+                    Value::Eight => "8",
+                }
+            }
+        };
+        return res;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,282 @@
-use yew::prelude::*;
-// use gloo_console as console;
+use cell::Cell;
+use yew::*;
+use gloo_console as console;
+use gloo::timers::callback::Interval;
+use rand::Rng;
+use yew::html::Scope;
+use std::collections::HashSet;
 
-pub trait Min {
-    fn min() -> Self;
-}
-
-pub trait Max {
-    fn max() -> Self;
-}
-
-impl Min for u8 {
-    fn min() -> u8 { std::u8::MIN }
-}
-
-impl Max for u8 {
-    fn max() -> u8 { std::u8::MAX }
-}
-
-impl Min for i8 {
-    fn min() -> i8 { std::i8::MIN }
-}
-
-impl Max for i8 {
-    fn max() -> i8 { std::i8::MAX }
-}
-
+mod cell;
 
 enum Msg {
-    AddOne,
-    SubtractOne,
+    Tick,
+    MouseDown,
+    Click(usize),
+    RightClick(usize),
+    // Chord(usize),
+    Reset,
 }
 
-struct Model {
-    value: i8,
+enum Face {
+    Happy,
+    Nervous,
+    Dead,
+    Cool,
 }
 
-impl Component for Model {
+impl Face {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Face::Happy => "🙂",
+            Face::Nervous => "😬",
+            Face::Dead => "😵",
+            Face::Cool => "😎",
+        }
+    }
+}
+
+struct App {
+    active:         bool,
+    face:           Face,
+    cells:          Vec<Cell>,
+    cells_width:    usize,
+    cells_height:   usize,
+    mines_count:     usize,
+    seconds_played: usize,
+    _interval:      Interval,
+}
+
+impl App {
+    fn reset(&mut self) {
+        let cells = generate_cells(self.cells_width, self.cells_height, self.mines_count);
+        self.reassign_cells(&cells);
+        self.seconds_played = 0;
+    }
+
+    fn reassign_cells(&mut self, cells: &Vec<Cell>) {
+        for (index, cell) in cells.iter().enumerate() {
+            self.cells[index] = *cell;
+        }
+    }
+
+    fn view_cell(&self, index: usize, cell: &Cell, link: &Scope<Self>) -> Html {
+        let is_shown = {
+            if cell.is_shown() { "clicked" } else { "" }
+        };
+        let is_mine = {
+            if cell.is_mine() { "mine" } else { "" }
+        };
+        let value = cell.get_value_string();
+
+        console::log!("is_mine, val", is_mine, value);
+        html! {
+            <td key={index}
+                class={classes!("cell", is_shown, is_mine)}
+                onclick={link.callback(move |_| Msg::Click(index))}
+            >
+                {value}
+            </td>
+        }
+    }
+
+}
+
+impl Component for App {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
+        console::log!("Building app...");
+        let callback = _ctx.link().callback(|_| Msg::Tick);
+        let interval = Interval::new(1000, move || callback.emit(()));
+
+        let (cells_width, cells_height, mines_count) = (100, 100, 10);
+        let cells = generate_cells(cells_width, cells_height, mines_count);
+
+
+        console::log!("Done");
         Self {
-            value: -120,
+            active: false,
+            face: Face::Happy,
+            cells,
+            cells_width,
+            cells_height,
+            mines_count,
+            seconds_played: 0,
+            _interval: interval,
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Msg) -> bool {
         match msg {
-            Msg::AddOne => {
-                if self.value >= <i8 as Max>::max() {
-                    self.value = <i8 as Min>::min();
-                } else {
-                    self.value += 1;
-                }
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            },
-            Msg::SubtractOne => {
-                if self.value <= <i8 as Min>::min() {
-                    self.value = <i8 as Max>::max();
-                } else {
-                    self.value -= 1;
-                }
+            Msg::MouseDown => {
+                if self.active { self.face = Face::Nervous; }
                 true
             }
+            Msg::Tick => {
+                // if self.active {
+                //     self.seconds_played += 1;
+                //     true
+                // } else {
+                    false
+                // }
+            },
+            Msg::Reset => {
+                self.active = false;
+                self.face = Face::Happy;
+                self.reset();
+                console::log!("Resetting...");
+                true
+            },
+            Msg::Click(index) => {
+                if !self.active { self.active = true; }
+
+                let mut cell = self.cells[index];
+                cell.handle_click();
+                self.cells[index] = cell; // Need to reassign cell or its changes aren't saved
+
+                if cell.is_mine() {
+                    self.active = false;
+                    self.face = Face::Dead;
+                } else {
+                    self.face = Face::Happy;
+                }
+                true
+            },
+            Msg::RightClick(index) => {
+                let mut cell = self.cells[index];
+                cell.cycle_display();
+                true
+            },
+            // Msg::Chord(_) => todo!(),
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
+        let cell_rows = self.cells
+            .chunks(self.cells_width)
+            .enumerate()
+            .map(|(y, cells)| {
+                let index_offset = y * self.cells_width;
+
+                let row_cells = cells
+                    .iter()
+                    .enumerate()
+                    .map(|(x, cell)| self.view_cell(index_offset + x, cell, ctx.link()));
+                html! {
+                    <tr key={y} class="game-row">
+                        { for row_cells }
+                    </tr>
+                }
+            });
+
         html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <button onclick={link.callback(|_| Msg::SubtractOne)}>{ "-1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <>
+                <div class="container no-select">
+                    <div class="header">
+                        <div id="minesRemainingContainer" class="counter left">
+                            <span id="minesRemaining"></span>
+                        </div>
+                        <div id="resetButtonContainer" class="center">
+                            <span id="resetButton" onclick={ctx.link().callback(move |_| Msg::Reset)}>{self.face.as_str()}</span>
+                        </div>
+                        <div id="timerContainer" class="counter right">
+                            <span id="timer">{"000"}</span>
+                        </div>
+                    </div>
+
+                    <table id="board" class="board" onmousedown={ctx.link().callback(move |_| Msg::MouseDown)}>
+                        { for cell_rows }
+                    </table>
+                </div>
+
+                // <script type="text/javascript">
+                //     // Disabling right click
+                //     document.oncontextmenu = function(event) { event.preventDefault(); };
+
+                //     window.game = new Game();
+                //     selectChanged()
+
+                //     function selectChanged() {
+                //         var difficulty = document.getElementById("difficulty").value;
+                //         game.setup(difficulty);
+                //     }
+                // </script>
+            </>
         }
     }
 }
 
+fn generate_cells(cells_width: usize, cells_height: usize, mines_count: usize) -> Vec<Cell> {
+    let mut cells: Vec<Cell> = Vec::new();
+    let mut mine_indicies: HashSet<usize> = HashSet::new();
+    for _ in 0..mines_count {
+        let mut i = get_random_cell_index(cells_width, cells_height);
+
+        while mine_indicies.contains(&i) {
+            i = get_random_cell_index(cells_width, cells_height);
+        }
+        mine_indicies.insert(i);
+        console::log!(i);
+    }
+
+    let cells_count = cells_width * cells_height;
+    for index in 0..cells_count {
+        let (row, col) = get_row_col_from_index(index as isize, cells_width as isize, cells_height as isize);
+        let neighbors = neighbors(row as isize, col as isize, cells_width as isize, cells_height as isize);
+        let value = Cell::calculate_value(index, &neighbors, &mine_indicies);
+        let cell = Cell::new(value);
+        cells.push(cell);
+    }
+
+    cells
+}
+
+fn get_random_cell_index(cells_width: usize, cells_height: usize) -> usize {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..(cells_width * cells_height))
+}
+
+fn neighbors(row: isize, col: isize, cells_width: isize, cells_height: isize) -> HashSet<usize> {
+    let mut neighbors: HashSet<usize> = HashSet::new();
+
+    neighbors.insert(get_index_from_row_col(row - 1, col - 1, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row - 1, col, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row - 1, col + 1, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row, col - 1, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row, col + 1, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row + 1, col - 1, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row + 1, col, cells_width, cells_height));
+    neighbors.insert(get_index_from_row_col(row + 1, col + 1, cells_width, cells_height));
+
+    neighbors
+}
+
+fn get_index_from_row_col(row: isize, col: isize, cells_width: isize, cells_height: isize) -> usize {
+    let row = clamp(row, cells_height);
+    let col = clamp(col, cells_width);
+
+    row * cells_width as usize + col
+}
+
+fn get_row_col_from_index(index: isize, cells_width: isize, cells_height: isize) -> (usize, usize) {
+    let row = index / cells_height;
+    let col = index % cells_width;
+
+    (row as usize, col as usize)
+}
+
+fn clamp(coord: isize, range: isize) -> usize {
+    let result = if coord < 0 {
+        0
+    } else if coord > range {
+        range
+    } else {
+        coord
+    };
+    result as usize
+}
+
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
